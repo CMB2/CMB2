cfd80fc75784f1fe2fd53220f3a9b9507e696567
Jared Atchison <contact@jaredatchison.com>
2011-03-24 19:10:33 -0500

    first commit

---
+++ README.txt
@@ -0,0 +1,30 @@
+=== Custom Metaboxes and Fields ===
+Contributors:	Andrew Norcross (@norcross / andrewnorcross.com)
+				Jared Atchison (@jaredatch / jaredatchison.com)
+				Bill Erickson (@billerickson / billerickson.net)
+Version: 0.4
+Requires at least: 3.0
+Tested up to: 3.1
+ 
+== Description ==
+
+This will create metaboxes with custom fields that will blow your mind.
+
+== Installation ==
+
+This script is easy to install. If you can't figure it out you probably shouldn't be using it.
+
+1. Place `metabox` directory inside a 'lib' directory of your (activated) theme. E.g. inside /themes/twentyten/lib/.
+2. Edit theme's function.php to include /lib/metabox/init.php.
+3. See example-functions.php for further guidance.
+
+== Frequently Asked Questions ==
+
+None yet.
+
+== Changelog ==
+
+= 0.4 =
+* Think we have a release that is mostly working. We'll say the initial release :)
+
+
---
+++ example-functions.php
@@ -0,0 +1,127 @@
+<?php
+
+// Include & setup custom metabox and fields
+$prefix = 'cmb_';
+$meta_boxes = array();
+
+$meta_boxes[] = array(
+    'id' => 'test_metabox',
+    'title' => 'Test Metabox',
+    'pages' => array('page'), // post type
+	'context' => 'normal',
+	'priority' => 'high',
+	'show_names' => true, // Show field names on the left
+    'fields' => array(
+        array(
+            'name' => 'Test Text',
+            'desc' => 'field description (optional)',
+            'id' => $prefix . 'test_text',
+            'type' => 'text'
+        ),
+		array(
+            'name' => 'Test Text Small',
+            'desc' => 'field description (optional)',
+            'id' => $prefix . 'test_textsmall',
+            'type' => 'text_small'
+        ),
+		array(
+            'name' => 'Test Text Medium',
+            'desc' => 'field description (optional)',
+            'id' => $prefix . 'test_textmedium',
+            'type' => 'text_medium'
+        ),
+		array(
+	        'name' => 'Test Date Picker',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_textdate',
+	        'type' => 'text_date'
+	    ),
+		array(
+	        'name' => 'Test Money',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_textmoney',
+	        'type' => 'text_money'
+	    ),
+	    array(
+	        'name' => 'Test Text Area',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_textarea',
+	        'type' => 'textarea'
+	    ),
+		array(
+	        'name' => 'Test Text Area Small',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_textareasmall',
+	        'type' => 'textarea_small'
+	    ),
+		array(
+	        'name' => 'Test Title Weeeee',
+	        'desc' => 'This is a title description',
+	        'type' => 'title'
+	    ),
+		array(
+		       'name' => 'Test Select',
+		       'desc' => 'field description (optional)',
+		       'id' => $prefix . 'test_select',
+		       'type' => 'select',
+				'options' => array(
+					array('name' => 'Option One', 'value' => 'standard'),
+					array('name' => 'Option Two', 'value' => 'custom'),
+					array('name' => 'Option Three', 'value' => 'none')				
+				)
+		),
+		array(
+	        'name' => 'Test Radio inline',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_radio',
+	        'type' => 'radio_inline',
+			'options' => array(
+				array('name' => 'Option One', 'value' => 'standard'),
+				array('name' => 'Option Two', 'value' => 'custom'),
+				array('name' => 'Option Three', 'value' => 'none')				
+			)
+	    ),
+		array(
+	        'name' => 'Test Radio',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_radio',
+	        'type' => 'radio',
+			'options' => array(
+				array('name' => 'Option One', 'value' => 'standard'),
+				array('name' => 'Option Two', 'value' => 'custom'),
+				array('name' => 'Option Three', 'value' => 'none')				
+			)
+	    ),
+		array(
+	        'name' => 'Test Checkbox',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_checkbox',
+	        'type' => 'checkbox'
+	    ),
+		array(
+	        'name' => 'Test Multi Checkbox',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_multicheckbox',
+	        'type' => 'multicheck',
+			'options' => array(
+				'check1' => 'Check One',
+				'check2' => 'Check Two',
+				'check3' => 'Check Three',
+			)
+	    ),
+		array(
+	        'name' => 'Test wysiwyg',
+	        'desc' => 'field description (optional)',
+	        'id' => $prefix . 'test_wysiwyg',
+	        'type' => 'wysiwyg'
+	    ),
+		array(
+	        'name' => 'Test Image',
+	        'desc' => 'Upload an image or enter an URL.',
+	        'id' => $prefix . 'test_image',
+	        'type' => 'file'
+	    ),
+    )
+);
+
+require_once('metabox/init.php');
