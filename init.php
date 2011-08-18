d31e27e0bd3a853e8a43430aa947ce8ecb9fe863
Randy Hoyt <randy@randyhoyt.com>
2011-08-18 00:03:28 -0500

    This one line of code will allow developers to use custom meta boxes in a theme *and* a plugin simultaneously. This is useful for distributing a plugin with the custom meta boxes library in it to someone else who might already be using the library.

--- init.php
+++ init.php
@@ -31,7 +31,7 @@ Version: 		0.5
 /************************************************************************
 		You should not edit the code below or things might explode!
 *************************************************************************/
-
+$meta_boxes = apply_filters ( 'cmb_meta_boxes' , $meta_boxes );
 foreach ( $meta_boxes as $meta_box ) {
 	$my_box = new cmb_Meta_Box( $meta_box );
 }
