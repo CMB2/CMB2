40b3163b42b4436e72032cedeca6289fa0b90251
Jared Atchison <contact@jaredatchison.com>
2011-10-19 16:23:38 -0500

    fixed $prefix not being accessible inside the function

--- example-functions.php
+++ example-functions.php
@@ -3,6 +3,7 @@
 $prefix = '_cmb_'; // start with an underscore to hide fields from custom fields list
 add_filter( 'cmb_meta_boxes', 'be_sample_metaboxes' );
 function be_sample_metaboxes( $meta_boxes ) {
+	global $prefix;
 	$meta_boxes[] = array(
 	    'id' => 'test_metabox',
 	    'title' => 'Test Metabox',
@@ -159,9 +160,9 @@ function be_sample_metaboxes( $meta_boxes ) {
 
 
 // Initialize the metabox class
-add_action('init','be_initialize_cmb_meta_boxes',9999);
+add_action( 'init', 'be_initialize_cmb_meta_boxes' ,9999 );
 function be_initialize_cmb_meta_boxes() {
-    if (!class_exists('cmb_Meta_Box')) {
-        require_once('init.php');
+    if ( !class_exists( 'cmb_Meta_Box' ) ) {
+        require_once( 'init.php' );
     }
 }
