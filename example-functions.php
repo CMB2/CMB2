83766b4bf3d105103959b509e114d0e4744f119c
Bill Erickson <bill.erickson@gmail.com>
2011-11-14 15:53:00 -0600

    Update example-functions.php

--- example-functions.php
+++ example-functions.php
@@ -170,10 +170,4 @@ function be_initialize_cmb_meta_boxes() {
 	if ( !class_exists( 'cmb_Meta_Box' ) ) {
 		require_once( 'init.php' );
 	}
-
-add_action( 'init', 'be_initialize_cmb_meta_boxes' ,9999 );
-function be_initialize_cmb_meta_boxes() {
-    if ( !class_exists( 'cmb_Meta_Box' ) ) {
-        require_once( 'init.php' );
-    }
 }
