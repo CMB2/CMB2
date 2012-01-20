4c683d857821304154abd4cc1a618040b671d0d9
jaredatch <contact@jaredatchison.com>
2012-01-19 18:54:57 -0600

    updated prefixes for consistency

--- example-functions.php
+++ example-functions.php
@@ -8,14 +8,14 @@
  * @link     https://github.com/jaredatch/Custom-Metaboxes-and-Fields-for-WordPress
  */
 
-add_filter( 'cmb_meta_boxes', 'be_sample_metaboxes' );
+add_filter( 'cmb_meta_boxes', 'cmb_sample_metaboxes' );
 /**
  * Define the metabox and field configurations.
  *
  * @param  array $meta_boxes
  * @return array
  */
-function be_sample_metaboxes( array $meta_boxes ) {
+function cmb_sample_metaboxes( array $meta_boxes ) {
 
 	// Start with an underscore to hide fields from custom fields list
 	$prefix = '_cmb_';
@@ -210,11 +210,11 @@ function be_sample_metaboxes( array $meta_boxes ) {
 	return $meta_boxes;
 }
 
-add_action( 'init', 'be_initialize_cmb_meta_boxes', 9999 );
+add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
 /**
  * Initialize the metabox class.
  */
-function be_initialize_cmb_meta_boxes() {
+function cmb_initialize_cmb_meta_boxes() {
 
 	if ( ! class_exists( 'cmb_Meta_Box' ) )
 		require_once 'init.php';
