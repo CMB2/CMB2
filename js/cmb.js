ad114a27ed2445ec30f5d60cbda6d2ddaa41db30
Justin Sternberg <justin@dsgnwrks.pro>
2013-09-21 11:47:41 -0400

    Use new media modal

--- js/cmb.js
+++ js/cmb.js
@@ -18,6 +18,8 @@ jQuery(document).ready(function ($) {
 	'use strict';
 
 	var formfield;
+	// Uploading files
+	var file_frame = false;
 
 	/**
 	 * Initialize timepicker (this will be moved inline in a future release)
@@ -64,79 +66,82 @@ jQuery(document).ready(function ($) {
 	/**
 	 * File and image upload handling
 	 */
-	$('.cmb_upload_file').change(function () {
+
+
+	$('.cmb_metabox')
+	.on( 'change', '.cmb_upload_file', function () {
 		formfield = $(this).attr('name');
 		$('#' + formfield + '_id').val("");
-	});
+	})
+	.on( 'click', '.cmb_upload_button', function (event) {
 
-	$('.cmb_upload_button').live('click', function () {
-		var buttonLabel;
-		formfield = $(this).prev('input').attr('name');
-		buttonLabel = 'Use as ' + $('label[for=' + formfield + ']').text();
-		tb_show('', 'media-upload.php?post_id=' + $('#post_ID').val() + '&type=file&cmb_force_send=true&cmb_send_label=' + buttonLabel + '&TB_iframe=true');
-		return false;
-	});
+		event.preventDefault();
 
-	$('.cmb_remove_file_button').live('click', function () {
-		formfield = $(this).attr('rel');
-		$('input#' + formfield).val('');
-		$('input#' + formfield + '_id').val('');
-		$(this).parent().remove();
-		return false;
-	});
+		var self = $(this);
+		formfield = self.prev('input').attr('name');
+		var $formfield = $('#'+formfield);
+		var uploadStatus = true;
+		var attachment = true;
 
-	window.original_send_to_editor = window.send_to_editor;
-    window.send_to_editor = function (html) {
-		var itemurl, itemclass, itemClassBits, itemid, htmlBits, itemtitle,
-			image, uploadStatus = true;
-
-		if (formfield) {
-
-	        if ($(html).html(html).find('img').length > 0) {
-				itemurl = $(html).html(html).find('img').attr('src'); // Use the URL to the size selected.
-				itemclass = $(html).html(html).find('img').attr('class'); // Extract the ID from the returned class name.
-				itemClassBits = itemclass.split(" ");
-				itemid = itemClassBits[itemClassBits.length - 1];
-				itemid = itemid.replace('wp-image-', '');
-	        } else {
-				// It's not an image. Get the URL to the file instead.
-				htmlBits = html.split("'"); // jQuery seems to strip out XHTML when assigning the string to an object. Use alternate method.
-				itemurl = htmlBits[1]; // Use the URL to the file.
-				itemtitle = htmlBits[2];
-				itemtitle = itemtitle.replace('>', '');
-				itemtitle = itemtitle.replace('</a>', '');
-				itemid = ""; // TO DO: Get ID for non-image attachments.
-			}
-
-			image = /(jpe?g|png|gif|ico)$/gi;
-
-			if (itemurl.match(image)) {
-				uploadStatus = '<div class="img_status"><img src="' + itemurl + '" alt="" /><a href="#" class="cmb_remove_file_button" rel="' + formfield + '">Remove Image</a></div>';
-			} else {
-				// No output preview if it's not an image
-				// Standard generic output if it's not an image.
-				html = '<a href="' + itemurl + '" target="_blank" rel="external">View File</a>';
-				uploadStatus = '<div class="no_image"><span class="file_link">' + html + '</span>&nbsp;&nbsp;&nbsp;<a href="#" class="cmb_remove_file_button" rel="' + formfield + '">Remove</a></div>';
-			}
-
-			$('#' + formfield).val(itemurl);
-			$('#' + formfield + '_id').val(itemid);
-			$('#' + formfield).siblings('.cmb_media_status').slideDown().html(uploadStatus);
-			tb_remove();
-
-		} else {
-			window.original_send_to_editor(html);
+		// If the media frame already exists, reopen it.
+		if ( file_frame ) {
+			file_frame.open();
+			return;
 		}
 
-		formfield = '';
-	};
+		// Create the media frame.
+		file_frame = wp.media.frames.file_frame = wp.media({
+			title: $('label[for=' + formfield + ']').text(),
+			button: {
+				text: window.cmb_l10.upload_file
+			},
+			multiple: false
+		});
+
+		// When an file is selected, run a callback.
+		file_frame.on( 'select', function() {
+
+			// Only get one file from the uploader
+			attachment = file_frame.state().get('selection').first().toJSON();
+
+			$formfield.val(attachment.url);
+			$('#'+ formfield +'_id').val(attachment.id);
+
+			if ( attachment.type && attachment.type === 'image' ) {
+				// image preview
+				uploadStatus = '<div class="img_status"><img style="max-width: 350px; width: 100%; height: auto;" src="' + attachment.url + '" alt="'+ attachment.filename +'" title="'+ attachment.filename +'" /><p><a href="#" class="cmb_remove_file_button" rel="' + formfield + '">'+ cmb_l10.remove_image +'</a></p></div>';
+			} else {
+				// Standard generic output if it's not an image.
+				uploadStatus = cmb_l10.file +' <strong>'+ attachment.filename +'</strong>&nbsp;&nbsp;&nbsp; (<a href="'+ attachment.url +'" target="_blank" rel="external">'+ cmb_l10.download +'</a> / <a href="#" class="cmb_remove_file_button" rel="'+ formfield +'">'+ cmb_l10.remove_file +'</a>)';
+			}
+			// add/display our output
+			$formfield.siblings('.cmb_media_status').slideDown().html(uploadStatus);
+		});
+
+		// Finally, open the modal
+		file_frame.open();
+	})
+	.on( 'click', '.cmb_remove_file_button', function (event) {
+		formfield = $(this).attr('rel');
+		var container = $(this).parents('.cmb_media_status');
+		$('input#' + formfield).val('');
+		$('input#' + formfield + '_id').val('');
+		container.html('');
+		return false;
+	})
 
 	/**
 	 * Ajax oEmbed display
 	 */
 
+	// ajax when typing
+	.on( 'keyup', '.cmb_oembed', function (event) {
+		// fire our ajax function
+		doCMBajax($(this), event);
+	});
+
 	// ajax on paste
-	$('.cmb_oembed').bind('paste', function (e) {
+	$('.cmb_oembed').bind( 'paste', function (e) {
 		var pasteitem = $(this);
 		// paste event is fired before the value is filled, so wait a bit
 		setTimeout(function () {
@@ -149,12 +154,6 @@ jQuery(document).ready(function ($) {
 			// if it's been 2 seconds, hide our spinner
 			$('.postbox table.cmb_metabox .cmb-spinner').hide();
 		}, 2000);
-	});
-
-	// ajax when typing
-	$('.cmb_metabox').on('keyup', '.cmb_oembed', function (event) {
-		// fire our ajax function
-		doCMBajax($(this), event);
 	});
 
 	// function for running our ajax
@@ -189,8 +188,9 @@ jQuery(document).ready(function ($) {
 							'oembed_url': oembed_url,
 							'oembed_width': obj.width(),
 							'field_id': field_id,
-							'post_id': window.cmb_ajax_data.post_id,
-							'cmb_ajax_nonce': window.cmb_ajax_data.ajax_nonce
+							'object_id': window.cmb_l10.object_id,
+							'object_type': window.cmb_l10.object_type,
+							'cmb_ajax_nonce': window.cmb_l10.ajax_nonce
 						},
 						success: function (response) {
 							// if we have a response id
