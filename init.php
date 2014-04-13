6499210784a0a52ae31ebb68a2fc18c1403284f0
Julien Maury <tweetpressfr@gmail.com>
2014-04-13 17:41:32 +0200

    Let WP translates "save"
    In each version on WP this term is translated.

--- init.php
+++ init.php
@@ -1152,7 +1152,7 @@ function cmb_metabox_form( $meta_box, $object_id, $echo = true ) {
 
 	$form_format = apply_filters( 'cmb_frontend_form_format', '<form class="cmb-form" method="post" id="%s" enctype="multipart/form-data" encoding="multipart/form-data"><input type="hidden" name="object_id" value="%s">%s<input type="submit" name="submit-cmb" value="%s" class="button-primary"></form>', $object_id, $meta_box, $form );
 
-	$form = sprintf( $form_format, $meta_box['id'], $object_id, $form, __( 'Save', 'cmb' ) );
+	$form = sprintf( $form_format, $meta_box['id'], $object_id, $form, __( 'Save') );
 
 	if ( $echo )
 		echo $form;
