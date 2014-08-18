bdc38ecd2b988e696df9e11c09594ce51fa5bdfe
Justin Sternberg <justin@dsgnwrks.pro>
2014-08-18 10:59:53 -0400

    wrap in class_exists check

--- init.php
+++ init.php
@@ -39,59 +39,63 @@
                   or things might explode!
 *************************************************************************/
 
-/**
- * Check for newest version of CMB
- */
-class cmb2_bootstrap_200 {
+if ( ! class_exists( 'cmb2_bootstrap_200' ) ) {
 
 	/**
-	 * Current version number
-	 * @var   string
-	 * @since 1.0.0
+	 * Check for newest version of CMB
 	 */
-	const VERSION = '2.0.0';
+	class cmb2_bootstrap_200 {
 
-	/**
-	 * Current version hook priority
-	 * Will decrement with each release
-	 *
-	 * @var   int
-	 * @since 2.0.0
-	 */
-	const PRIORITY = 9999;
+		/**
+		 * Current version number
+		 * @var   string
+		 * @since 1.0.0
+		 */
+		const VERSION = '2.0.0';
 
-	public static $single = null;
+		/**
+		 * Current version hook priority
+		 * Will decrement with each release
+		 *
+		 * @var   int
+		 * @since 2.0.0
+		 */
+		const PRIORITY = 9999;
 
-	public static function go() {
-		if ( null === self::$single ) {
-			self::$single = new self();
-		}
-		return self::$single;
-	}
+		public static $single = null;
 
-	private function __construct() {
-		add_action( 'init', array( $this, 'include_cmb' ), self::PRIORITY );
-	}
-
-	public function include_cmb() {
-		if ( ! class_exists( 'CMB2' ) ) {
-			if ( ! defined( 'CMB2_VERSION' ) ) {
-				define( 'CMB2_VERSION', self::VERSION );
+		public static function go() {
+			if ( null === self::$single ) {
+				self::$single = new self();
 			}
-			$this->l10ni18n();
-			require_once 'CMB2.php';
+			return self::$single;
 		}
-	}
 
-	/**
-	 * Load CMB text domain
-	 * @since  2.0.0
-	 */
-	public function l10ni18n() {
-		$locale = apply_filters( 'plugin_locale', get_locale(), 'cmb2' );
-		load_textdomain( 'cmb2', WP_LANG_DIR . '/cmb2/cmb2-' . $locale . '.mo' );
-		load_plugin_textdomain( 'cmb2', false, dirname( __FILE__ ) . '/languages/' );
-	}
+		private function __construct() {
+			add_action( 'init', array( $this, 'include_cmb' ), self::PRIORITY );
+		}
 
-}
-cmb2_bootstrap_200::go();
+		public function include_cmb() {
+			if ( ! class_exists( 'CMB2' ) ) {
+				if ( ! defined( 'CMB2_VERSION' ) ) {
+					define( 'CMB2_VERSION', self::VERSION );
+				}
+				$this->l10ni18n();
+				require_once 'CMB2.php';
+			}
+		}
+
+		/**
+		 * Load CMB text domain
+		 * @since  2.0.0
+		 */
+		public function l10ni18n() {
+			$locale = apply_filters( 'plugin_locale', get_locale(), 'cmb2' );
+			load_textdomain( 'cmb2', WP_LANG_DIR . '/cmb2/cmb2-' . $locale . '.mo' );
+			load_plugin_textdomain( 'cmb2', false, dirname( __FILE__ ) . '/languages/' );
+		}
+
+	}
+	cmb2_bootstrap_200::go();
+
+} // class exists check
