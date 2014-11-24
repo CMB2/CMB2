b6b8937a2435912eb3caed2895d8a7ccbf69443e
Greg Rickaby <gregrickaby@gmail.com>
2014-11-24 10:13:05 -0600

    added spaces

--- example-functions.php
+++ example-functions.php
@@ -11,10 +11,10 @@
 /**
  * Get the bootstrap! If using the plugin from wordpress.org, REMOVE THIS!
  */
-if ( file_exists(  __DIR__ .'/cmb2/init.php' ) ) {
-	require_once  __DIR__ .'/cmb2/init.php';
-} elseif ( file_exists(  __DIR__ .'/CMB2/init.php' ) ) {
-	require_once  __DIR__ .'/CMB2/init.php';
+if ( file_exists(  __DIR__ . '/cmb2/init.php' ) ) {
+	require_once  __DIR__ . '/cmb2/init.php';
+} elseif ( file_exists(  __DIR__ . '/CMB2/init.php' ) ) {
+	require_once  __DIR__ . '/CMB2/init.php';
 }
 
 /**
