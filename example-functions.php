c014c7f3d7a361349bb4e3b414d94a3e0f9205a2
Bryce <brycead@gmail.com>
2014-12-23 09:46:05 +0700

    Use only dirname( __FILE__ )

--- example-functions.php
+++ example-functions.php
@@ -12,13 +12,10 @@
  * Get the bootstrap! If using the plugin from wordpress.org, REMOVE THIS!
  */
 
-// Check for  PHP version and use the correct dir
-$dir = ( version_compare( PHP_VERSION, '5.3.0' ) >= 0 ) ? __DIR__ : dirname( __FILE__ );
-
-if ( file_exists(  $dir . '/cmb2/init.php' ) ) {
-	require_once  $dir . '/cmb2/init.php';
-} elseif ( file_exists(  $dir . '/CMB2/init.php' ) ) {
-	require_once  $dir . '/CMB2/init.php';
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/CMB2/init.php';
 }
 
 /**
