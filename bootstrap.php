80b46bc40e382dc852da1eaa4c33c8735b2ad877
Justin Sternberg <justin@dsgnwrks.pro>
2015-02-14 11:54:03 -0500

    Add cmb2_init_before_hookup hook for modifying registered metaboxes before hookup

--- bootstrap.php
+++ bootstrap.php
@@ -22,6 +22,8 @@ require_once cmb2_dir( 'includes/helper-functions.php' );
 
 /**
  * Fires when CMB2 is included/loaded
+ *
+ * Should be used to to add metaboxes. See example-functions.php
  */
 do_action( 'cmb2_init' );
 
@@ -36,6 +38,11 @@ foreach ( (array) $all_meta_boxes_config as $meta_box_config ) {
 }
 
 /**
+ * Fires after all CMB2 instances are created
+ */
+do_action( 'cmb2_init_before_hookup' );
+
+/**
  * Get all created metaboxes, and instantiate CMB2_hookup
  * on metaboxes which require it.
  * @since  2.0.2
@@ -47,7 +54,7 @@ foreach ( CMB2_Boxes::get_all() as $cmb ) {
 }
 
 /**
- * Fires after CMB2 instances are created
+ * Fires after CMB2 initiation process has been completed
  */
 do_action( 'cmb2_after_init' );
 
