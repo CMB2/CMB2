208e6c171fc9fd8a5de0bcd8ecbaa31c352ca2a8
Justin Sternberg <justin@dsgnwrks.pro>
2015-02-17 15:28:42 -0500

    update assertHTMLstringsAreEqual test to output a meaningful string comparison

--- tests/cmb-tests-base.php
+++ tests/cmb-tests-base.php
@@ -14,7 +14,28 @@ abstract class CMB2_Test extends WP_UnitTestCase {
 	}
 
 	public function assertHTMLstringsAreEqual( $expected_string, $string_to_test ) {
-		return $this->assertEquals( $this->normalize_string( $expected_string ), $this->normalize_string( $string_to_test ) );
+		$expected_string = $this->normalize_string( $expected_string );
+		$string_to_test = $this->normalize_string( $string_to_test );
+
+		$compare = strcmp( $expected_string, $string_to_test );
+
+		if ( 0 !== $compare ) {
+
+			$compare       = strspn( $expected_string ^ $string_to_test, "\0" );
+			$chars_to_show = 50;
+			$start         = ( $compare - 5 );
+			$pointer       = '|--->>';
+			$sep           = "\n". str_repeat( '-', 75 );
+
+			$compare = sprintf(
+			    $sep . "\nFirst difference at position %d:\n\n  Expected: \t%s\n  Actual: \t%s\n" . $sep,
+			    $compare,
+			    substr( $expected_string, $start, 5 ) . $pointer . substr( $expected_string, $compare, $chars_to_show ),
+			    substr( $string_to_test, $start, 5 ) . $pointer . substr( $string_to_test, $compare, $chars_to_show )
+			);
+		}
+
+		return $this->assertEquals( $expected_string, $string_to_test, ! empty( $compare ) ? $compare : null );
 	}
 
 	public function assertIsDefined( $definition ) {
