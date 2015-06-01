244d39fc3c27cfc195e495ecb331fac73e51399a
Justin Sternberg <justin@dsgnwrks.pro>
2015-06-01 08:10:53 -0400

    Add missing colorpicker dependency. Fixes #333, closes #336

--- includes/CMB2_Types.php
+++ includes/CMB2_Types.php
@@ -629,6 +629,7 @@ class CMB2_Types {
 		}
 
 		wp_enqueue_style( 'wp-color-picker' );
+		CMB2_JS::add_dependencies( array( 'wp-color-picker' ) );
 
 		return $this->input( array( 'class' => 'cmb2-colorpicker cmb2-text-small', 'value' => $meta_value ) );
 	}
