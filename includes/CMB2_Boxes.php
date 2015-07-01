5a4146eec546089fbe1a1c859d680dfda3a86ee2
Jeremy Pry <jeremy.pry@gmail.com>
2015-07-01 11:19:28 -0400

    Clean up CMB2_Boxes docblocks

--- includes/CMB2_Boxes.php
+++ includes/CMB2_Boxes.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * A CMB2 object instance registry for storing every CMB2 instance.
  *
@@ -11,25 +12,30 @@
 class CMB2_Boxes {
 
 	/**
-	 * Array of all metabox objects
-	 * @var   array
+	 * Array of all metabox objects.
+	 *
 	 * @since 2.0.0
+	 * @var array
 	 */
 	protected static $cmb2_instances = array();
 
 	/**
-	 * Add a CMB2 instance object to the registry
+	 * Add a CMB2 instance object to the registry.
+	 *
 	 * @since 1.X.X
-	 * @param CMB2  $cmb_instance CMB2 instance
+	 *
+	 * @param CMB2 $cmb_instance CMB2 instance.
 	 */
 	public static function add( CMB2 $cmb_instance ) {
 		self::$cmb2_instances[ $cmb_instance->cmb_id ] = $cmb_instance;
 	}
 
 	/**
-	 * Remove a CMB2 instance object to the registry
-	 * @since  1.X.X
-	 * @param  string $cmb_id A CMB2 instance id
+	 * Remove a CMB2 instance object to the registry.
+	 *
+	 * @since 1.X.X
+	 *
+	 * @param string $cmb_id A CMB2 instance id.
 	 */
 	public static function remove( $cmb_id ) {
 		if ( array_key_exists( $cmb_id, self::$cmb2_instances ) ) {
@@ -38,11 +44,13 @@ class CMB2_Boxes {
 	}
 
 	/**
-	 * Retrieve a CMB2 instance by cmb id
-	 * @since  1.X.X
-	 * @param  string $cmb_id A CMB2 instance id
+	 * Retrieve a CMB2 instance by cmb id.
 	 *
-	 * @return mixed          False or CMB2 object instance
+	 * @since 1.X.X
+	 *
+	 * @param string $cmb_id A CMB2 instance id.
+	 *
+	 * @return CMB2|bool False or CMB2 object instance.
 	 */
 	public static function get( $cmb_id ) {
 		if ( empty( self::$cmb2_instances ) || empty( self::$cmb2_instances[ $cmb_id ] ) ) {
@@ -53,9 +61,10 @@ class CMB2_Boxes {
 	}
 
 	/**
-	 * Retrieve all CMB2 instances registered
+	 * Retrieve all CMB2 instances registered.
+	 *
 	 * @since  1.X.X
-	 * @return array Array of all registered metaboxes
+	 * @return CMB2[] Array of all registered metaboxes.
 	 */
 	public static function get_all() {
 		return self::$cmb2_instances;
