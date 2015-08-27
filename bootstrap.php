2e9fc3faf42ceb407cd085874a0dba1092a2d2b6
Scott Kingsley Clark <lol@scottkclark.com>
2015-08-27 13:43:41 -0500

    Changes from @pglewis

--- bootstrap.php
+++ bootstrap.php
@@ -14,37 +14,39 @@
  *
  * Should be used to add metaboxes. See example-functions.php
  */
-do_action( 'cmb2_init' );
+if ( !defined('DOING_AJAX') ||  !DOING_AJAX ) {
+	do_action( 'cmb2_init' );
 
-/**
- * For back-compat. Does the dirtywork of instantiating all the
- * CMB2 instances for the cmb2_meta_boxes filter
- * @since  2.0.2
- */
-$all_meta_boxes_config = apply_filters( 'cmb2_meta_boxes', array() );
-foreach ( (array) $all_meta_boxes_config as $meta_box_config ) {
-	new CMB2( $meta_box_config );
-}
-
-/**
- * Fires after all CMB2 instances are created
- */
-do_action( 'cmb2_init_before_hookup' );
-
-/**
- * Get all created metaboxes, and instantiate CMB2_hookup
- * on metaboxes which require it.
- * @since  2.0.2
- */
-foreach ( CMB2_Boxes::get_all() as $cmb ) {
-	if ( $cmb->prop( 'hookup' ) ) {
-		$hookup = new CMB2_hookup( $cmb );
+	/**
+	 * For back-compat. Does the dirtywork of instantiating all the
+	 * CMB2 instances for the cmb2_meta_boxes filter
+	 * @since  2.0.2
+	 */
+	$all_meta_boxes_config = apply_filters( 'cmb2_meta_boxes', array() );
+	foreach ( (array) $all_meta_boxes_config as $meta_box_config ) {
+		new CMB2( $meta_box_config );
 	}
-}
 
-/**
- * Fires after CMB2 initiation process has been completed
- */
-do_action( 'cmb2_after_init' );
+	/**
+	 * Fires after all CMB2 instances are created
+	 */
+	do_action( 'cmb2_init_before_hookup' );
+
+	/**
+	 * Get all created metaboxes, and instantiate CMB2_hookup
+	 * on metaboxes which require it.
+	 * @since  2.0.2
+	 */
+	foreach ( CMB2_Boxes::get_all() as $cmb ) {
+		if ( $cmb->prop( 'hookup' ) ) {
+			$hookup = new CMB2_hookup( $cmb );
+		}
+	}
+
+	/**
+	 * Fires after CMB2 initiation process has been completed
+	 */
+	do_action( 'cmb2_after_init' );
+}
 
 // End. That's it, folks! //
