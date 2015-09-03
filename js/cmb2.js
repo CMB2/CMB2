177a3946fbdfd6d9901e359b3149c87d8df6d1cc
Justin Sternberg <justin@dsgnwrks.pro>
2015-09-03 13:24:49 -0400

    Do not toggle disabled checkboxes

--- js/cmb2.js
+++ js/cmb2.js
@@ -128,7 +128,7 @@ window.CMB2 = (function(window, document, $, undefined){
 	cmb.toggleCheckBoxes = function( evt ) {
 		evt.preventDefault();
 		var $self = $(this);
-		var $multicheck = $self.closest( '.cmb-td' ).find( 'input[type=checkbox]' );
+		var $multicheck = $self.closest( '.cmb-td' ).find( 'input[type=checkbox]:not([disabled])' );
 
 		// If the button has already been clicked once...
 		if ( $self.data( 'checked' ) ) {
