fddb1d7dac6e228fda594672dcd1814c1e420452
Justin Sternberg <justin@dsgnwrks.pro>
2015-09-09 15:08:09 -0400

    Clean things up a bit and make a little more readable

--- includes/CMB2_Utils.php
+++ includes/CMB2_Utils.php
@@ -28,33 +28,43 @@ class CMB2_Utils {
 	public function image_id_from_url( $img_url ) {
 		$attachment_id = 0;
 		$dir = wp_upload_dir();
-		if ( false !== strpos( $img_url, $dir['baseurl'] . '/' ) ) { // Is URL in uploads directory?
-			$file = basename( $img_url );
-			$query_args = array(
-				'post_type'   => 'attachment',
-				'post_status' => 'inherit',
-				'fields'      => 'ids',
-				'meta_query'  => array(
-					array(
-						'value'   => $file,
-						'compare' => 'LIKE',
-						'key'     => '_wp_attachment_metadata',
-					),
-				)
-			);
-			$query = new WP_Query( $query_args );
-			if ( $query->have_posts() ) {
-				foreach ( $query->posts as $post_id ) {
-					$meta = wp_get_attachment_metadata( $post_id );
-					$original_file       = basename( $meta['file'] );
-					$cropped_image_files = wp_list_pluck( $meta['sizes'], 'file' );
-					if ( $original_file === $file || in_array( $file, $cropped_image_files ) ) {
-						$attachment_id = $post_id;
-						break;
-					}
+
+		// Is URL in uploads directory?
+		if ( false === strpos( $img_url, $dir['baseurl'] . '/' ) ) {
+			return false;
+		}
+
+		$file = basename( $img_url );
+
+		$query_args = array(
+			'post_type'   => 'attachment',
+			'post_status' => 'inherit',
+			'fields'      => 'ids',
+			'meta_query'  => array(
+				array(
+					'value'   => $file,
+					'compare' => 'LIKE',
+					'key'     => '_wp_attachment_metadata',
+				),
+			)
+		);
+
+		$query = new WP_Query( $query_args );
+
+		if ( $query->have_posts() ) {
+
+			foreach ( $query->posts as $post_id ) {
+				$meta = wp_get_attachment_metadata( $post_id );
+				$original_file       = basename( $meta['file'] );
+				$cropped_image_files = wp_list_pluck( $meta['sizes'], 'file' );
+				if ( $original_file === $file || in_array( $file, $cropped_image_files ) ) {
+					$attachment_id = $post_id;
+					break;
 				}
 			}
+
 		}
+
 		return 0 === $attachment_id ? false : $attachment_id;
 	}
 
