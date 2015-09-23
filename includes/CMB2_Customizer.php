100cc49e77d20df43c429b2407be8e18e214108c
Ronald Huereca <rhuereca@bigwing.com>
2015-09-23 12:49:28 -0500

    Code formatting.

--- includes/CMB2_Customizer.php
+++ includes/CMB2_Customizer.php
@@ -8,61 +8,13 @@ class CMB2_Customizer {
     
     public function start_customizer( $wp_customize ) {
     
-    $customizer_objects = array();
-    $customizer_boxes = CMB2_Boxes::get_all();
-    foreach( $customizer_boxes as $type => $instance ) {
-        $customizer_objects[] = cmb2_get_metabox( $type, 0, 'customizer' );
-    }
-    
-    /*
-    $wp_customize->add_section( 
-    	'cmb_address_options', 
-    	array(
-    		'title'       => __( 'Address Settings', 'mytheme' ),
-    		'description' => __('Address Settings', 'mytheme'), 
-    		'priority'    => 800,
-    	) 
-    );
-    $wp_customize->add_setting( 'nest_street',
-	array(
-    	'default' => '',
-		'type' => 'option',
-		'sanitize_callback' => 'sanitize_text_field'
-        )
-    );
-    $wp_customize->add_setting( 'nest_newline',
-	array(
-    	'default' => '',
-		'type' => 'theme_mod',
-		'sanitize_callback' => 'sanitize_text_field'
-        )
-    );
-    
-    
-    $wp_customize->add_control( new WP_Customize_Control( 
-    	$wp_customize, 
-    	'nest_street',
-    	array(
-    		'label'    => 'Street Address', 
-    		'section'  => 'cmb_address_options',
-    		'settings' => 'nest_street',
-    		'priority' => 10,
-    		'type' => 'text'
-    	)
-    )); 
-    $wp_customize->add_control( new WP_Customize_Control( 
-    	$wp_customize, 
-    	'nest_street-1',
-    	array(
-    		'label'    => 'Street Address', 
-    		'section'  => 'cmb_address_options',
-    		'settings' => 'nest_newline',
-    		'priority' => 12,
-    		'type' => 'text'
-    	)
-    ));
-        return;*/
+        $customizer_objects = array();
+        $customizer_boxes = CMB2_Boxes::get_all();
+        foreach( $customizer_boxes as $type => $instance ) {
+            $customizer_objects[] = cmb2_get_metabox( $type, 0, 'customizer' );
+        }
         
+            
         foreach( $customizer_objects as $index => $cmb ) {
             /* Add Address Info to Customizer */
             $customizer_id = $cmb->prop( 'id' );
@@ -94,26 +46,7 @@ class CMB2_Customizer {
                 	)
                 ));
             
-        }
-        
-        
-        
-        
-        
-            /*(
-                $wp_customize->add_control( new WP_Customize_Control( 
-    	$wp_customize, 
-    	'nest_street_address',
-    	array(
-    		'label'    => 'Street Address', 
-    		'section'  => 'nest_address_options',
-    		'settings' => 'nest_street',
-    		'priority' => 10,
-    	)
-    ));
-    */
-        }
-        
-        
+            }
+        }    
     }
 }
