d2cefab0a8f9c7dba0b5866b191b11d2eb0cff71
Ronald Huereca <rhuereca@bigwing.com>
2015-09-24 20:12:04 -0500

    Getting radio boxes working.

--- includes/customizer/CMB2_Customizer_Radio.php
+++ includes/customizer/CMB2_Customizer_Radio.php
@@ -3,32 +3,17 @@
 class CMB_Customize_Radio extends WP_Customize_Control {
     public $type = 'radio';
     
-    public function render_content() {
-       /* Need help with this so we can use CMB2 native functions */
-       $field = new CMB2_Field( array( 'object_id' => $this->id, 'object_type' => 'radio', 'field_args' => array( 'id' => $this->id, 'type' => 'checkbox', 'name' => $this->id, 'value' => $this->value(), 'desc' => $this->label, 'escaped_value' => esc_html( $this->value() ) ) ) );
-       
-       
-       /*
-           'type'  => 'radio',
-			'class' => 'cmb2-option',
-			'name'  => $this->_name(),
-			'id'    => $this->_id( $i ),
-			'value' => $this->field->escaped_value(),
-			'label' => '',
-        */
-       
-       $type = new CMB2_Types( $field );
-       $radio = array();
+    public function render_content() {       
+       ob_start();
        $counter = 0;
-       foreach( $this->input_attrs as $label => $value) {
-           
-           $radio[] = sprintf( '<li><input type="radio" value="%s" id="%s" name="%s[]" %s />&nbsp;<label for="%s">%s</label></li>', esc_html( $this->value() ), esc_attr( $this->id . '_' . $counter ), esc_attr( $this->id ), checked( true, true, false ), esc_attr( $this->id . '_' . $counter ), esc_html( $value ) );
-           $counter += 1;
-               
+       echo sprintf( '<div id="customize-control-%s" class="customize-control customize-control-radio" style="display: list-item;">', esc_attr( $this->id ) );
+       echo sprintf( '<span class="customize-control-title">%s</span>', esc_html( $this->label ) );
+
+       foreach( $this->input_attrs as $label => $value ) {
+           printf( '<label for="%1$s"><input type="radio" value="%2$s" id="%1$s" name="%3$s" %4$s data-customize-setting-link="%6$s" />&nbsp;%5$s</label></li>', esc_attr( $this->id . '_' . $counter ), esc_html( $value ), '_' . esc_attr( $this->id ), checked( $value, $this->value(), false ), esc_html( $value ), esc_attr( $this->id ) );
+           echo '<br />';
+           $counter += 1;   
        }
-       
-       $radio = implode( '', $radio );
-       echo $type->radio( array( 'options' => $radio ), 'radio' );
-       return; 
+       echo '</div>';
     }
 }
