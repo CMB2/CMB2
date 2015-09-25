983e338db5ed5c446ea0252a0add70945f8654c2
Ronald Huereca <rhuereca@bigwing.com>
2015-09-25 08:39:02 -0500

    Fixing more html in timer.

--- includes/customizer/CMB2_Customizer_Text_Time.php
+++ includes/customizer/CMB2_Customizer_Text_Time.php
@@ -3,7 +3,9 @@
 class CMB_Customize_Text_Time extends WP_Customize_Control {    
     public function render_content() {
        /* Need help with this so we can use CMB2 native functions */
-       printf( '<input type="text" value="%2$s" class="cmb2-timepicker text-time hasDatepicker" id="%1$s" name="%1$s" data-customize-setting-link="%1$s" " />', $this->id , esc_html( $this->value() ) );        
+       echo '<div class="custom-cmb-timepicker">';
+       printf( '<input type="text" value="%2$s" class="cmb2-timepicker text-time" id="%1$s" name="%1$s" data-customize-setting-link="%1$s"/>', $this->id , esc_html( $this->value() ) );        
+       echo '</div>';
        return; 
     }
 }
