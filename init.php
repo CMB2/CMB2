2278ca5dadb301bc255046804184c3eb68614536
Justin Sternberg <justin@dsgnwrks.pro>
2015-10-01 09:18:35 -0400

    Switch class name for trunk

--- init.php
+++ init.php
@@ -48,7 +48,7 @@
                   or things might explode!
 *************************************************************************/
 
-if ( ! class_exists( 'CMB2_Bootstrap_212', false ) ) {
+if ( ! class_exists( 'CMB2_Bootstrap_213_Trunk', false ) ) {
 
 	/**
 	 * Handles checking for and loading the newest version of CMB2
@@ -61,7 +61,7 @@ if ( ! class_exists( 'CMB2_Bootstrap_212', false ) ) {
 	 * @license   GPL-2.0+
 	 * @link      http://webdevstudios.com
 	 */
-	class CMB2_Bootstrap_212 {
+	class CMB2_Bootstrap_213_Trunk {
 
 		/**
 		 * Current version number
@@ -77,20 +77,20 @@ if ( ! class_exists( 'CMB2_Bootstrap_212', false ) ) {
 		 * @var   int
 		 * @since 2.0.0
 		 */
-		const PRIORITY = 9987;
+		const PRIORITY = 9986;
 
 		/**
-		 * Single instance of the CMB2_Bootstrap_212 object
+		 * Single instance of the CMB2_Bootstrap_213_Trunk object
 		 *
-		 * @var CMB2_Bootstrap_212
+		 * @var CMB2_Bootstrap_213_Trunk
 		 */
 		public static $single_instance = null;
 
 		/**
-		 * Creates/returns the single instance CMB2_Bootstrap_212 object
+		 * Creates/returns the single instance CMB2_Bootstrap_213_Trunk object
 		 *
 		 * @since  2.0.0
-		 * @return CMB2_Bootstrap_212 Single instance object
+		 * @return CMB2_Bootstrap_213_Trunk Single instance object
 		 */
 		public static function initiate() {
 			if ( null === self::$single_instance ) {
@@ -177,6 +177,6 @@ if ( ! class_exists( 'CMB2_Bootstrap_212', false ) ) {
 	}
 
 	// Make it so...
-	CMB2_Bootstrap_212::initiate();
+	CMB2_Bootstrap_213_Trunk::initiate();
 
 }
