951366028b9e81069b62a0ade1218e6a089e6f58
Travis Smith <t@wpsmith.net>
2015-10-12 14:27:48 -0400

    change filter to filter type, not ID

--- includes/CMB2_Field.php
+++ includes/CMB2_Field.php
@@ -529,7 +529,7 @@ class CMB2_Field {
 		 * @param string            $action  Action performed. Could be "repeatable", "updated", or "removed".
 		 * @param CMB2_Field object $field   This field object
 		 */
-		do_action( "cmb2_save_field_{$field_id}", $updated, $action, $this );
+		do_action( "cmb2_save_field_{$this->args['type']}", $updated, $action, $new_value, $this );
 
 		return $updated;
 	}
