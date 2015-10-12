aeafbc1d74e2b1076e3595aaa817f1e2a5469d5a
Travis Smith <t@wpsmith.net>
2015-10-12 14:27:21 -0400

    Add new value

--- includes/CMB2_Field.php
+++ includes/CMB2_Field.php
@@ -515,7 +515,7 @@ class CMB2_Field {
 		 * @param string            $action   Action performed. Could be "repeatable", "updated", or "removed".
 		 * @param CMB2_Field object $field    This field object
 		 */
-		do_action( 'cmb2_save_field', $field_id, $updated, $action, $this );
+		do_action( 'cmb2_save_field', $field_id, $updated, $action, $new_value, $this );
 
 		/**
 		 * Hooks after save field action.
