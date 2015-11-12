2e32e7fab2f2666a407e28248b6be2d893ef4c80
Justin Sternberg <justin@dsgnwrks.pro>
2015-11-11 23:46:21 -0500

    init.php is not for hooking in. We'll do that later

--- init.php
+++ init.php
@@ -152,9 +152,6 @@ if ( ! class_exists( 'CMB2_Bootstrap_223_Trunk', false ) ) {
 			// Kick the whole thing off
 			require_once 'bootstrap.php';
 			cmb2_bootstrap();
-
-			// hook up the CMB rest endpoint class
-			add_action( 'rest_api_init', array( $this, 'cmb2_rest_routes' ), 0 );
 		}
 
 		/**
@@ -179,18 +176,6 @@ if ( ! class_exists( 'CMB2_Bootstrap_223_Trunk', false ) ) {
 				load_textdomain( 'cmb2', $mofile );
 			}
 
-		}
-
-
-		/**
-		 * cmb2_rest_routes function.
-		 *
-		 * @access public
-		 * @return void
-		 */
-		public function cmb2_rest_routes() {
-			$controller = new CMB2_REST_Endpoints;
-			$controller->register_routes();
 		}
 
 	}
