e4c2843a485f80dd2d65ae91ddf6a6de25438764
Christopher Olbekson <chris.olbekson@webdevstudios.com>
2016-02-19 10:34:42 -0600

    Adding field param option to remove WP default tax meta boxes

--- includes/CMB2_hookup.php
+++ includes/CMB2_hookup.php
@@ -242,6 +242,19 @@ class CMB2_hookup {
 					add_filter( "postbox_classes_{$post_type}_{$this->cmb->cmb_id}", array( $this, 'close_metabox_class' ) );
 				}
 
+				foreach ( $this->cmb->prop( 'fields' ) as $meta_box => $field ) {
+					if ( isset( $field['taxonomy'] ) && ! empty( $field['remove_default'] ) ) {
+						if ( ! is_taxonomy( $field['taxonomy'] ) ) {
+							continue;
+						}
+						if ( is_taxonomy_hierarchical( $field['taxonomy'] ) ) {
+							remove_meta_box( $field['taxonomy'] . 'div', $post_type, 'side' );
+						} else {
+							remove_meta_box( 'tagsdiv-' . $field['taxnomy'], $post_type, 'side' );
+						}
+					}
+				}
+
 				add_meta_box( $this->cmb->cmb_id, $this->cmb->prop( 'title' ), array( $this, 'metabox_callback' ), $post_type, $this->cmb->prop( 'context' ), $this->cmb->prop( 'priority' ) );
 			}
 		}
