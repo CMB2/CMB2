30aa4fcccd3b392581454b7175cc2250328514db
Justin Sternberg <justin@dsgnwrks.pro>
2016-09-09 00:47:44 -0500

    Attempt to fix rest-api plugin install for travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,22 +12,24 @@
  */
 
 if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	$test_root = getenv( 'WP_DEVELOP_DIR' );
+	$GLOBALS['test_root'] = getenv( 'WP_DEVELOP_DIR' );
 } elseif ( false !== getenv( 'WP_TESTS_DIR' ) ) {
-	$test_root = getenv( 'WP_TESTS_DIR' );
+	$GLOBALS['test_root'] = getenv( 'WP_TESTS_DIR' );
 } elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
-	$test_root = '../../../../tests/phpunit';
+	$GLOBALS['test_root'] = '../../../../tests/phpunit';
 } elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
-	$test_root = '/tmp/wordpress-tests-lib';
+	$GLOBALS['test_root'] = '/tmp/wordpress-tests-lib';
 }
 
-require_once $test_root . '/includes/functions.php';
+require_once $GLOBALS['test_root'] . '/includes/functions.php';
 
 /**
  * Activates the CMB2 plugin in WordPress so it can be tested.
  *
  */
 function _tests_cmb2_manually_load_plugin() {
+	global $test_root;
+
 	define( 'CMB2_TESTDATA', dirname( __FILE__ ) . '/data' );
 	if ( ! defined( 'WP_ADMIN' ) ) {
 		define( 'WP_ADMIN', true );
@@ -35,18 +37,27 @@ function _tests_cmb2_manually_load_plugin() {
 
 	$plugin = dirname( dirname( __FILE__ ) );
 	$wp_content = dirname( dirname( $plugin ) );
-	$rest_api = $wp_content . '/plugins/WP-API/plugin.php';
-	if ( file_exists( $rest_api ) ) {
+
+	if (
+		( $rest_api = $wp_content . '/plugins/WP-API/plugin.php' )
+		&& file_exists( $rest_api )
+	) {
 		require_once $rest_api;
-	} else {
-		$rest_api = $wp_content . '/plugins/rest-api/plugin.php';
-		if ( file_exists( $rest_api ) ) {
-			require_once $rest_api;
-		}
+	} elseif (
+		( $rest_api = $wp_content . '/plugins/rest-api/plugin.php' )
+		&& file_exists( $rest_api )
+	) {
+		require_once $rest_api;
+	} elseif (
+		( $rest_api = dirname( $test_root ) . '/wp-content/plugins/rest-api/plugin.php' )
+		&& file_exists( $rest_api )
+	) {
+		require_once $rest_api;
 	}
+
 	require_once $plugin . '/init.php';
 }
 tests_add_filter( 'muplugins_loaded', '_tests_cmb2_manually_load_plugin' );
 
-require $test_root . '/includes/bootstrap.php';
+require $GLOBALS['test_root'] . '/includes/bootstrap.php';
 
