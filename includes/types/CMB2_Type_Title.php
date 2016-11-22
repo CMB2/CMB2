b7e3cffd50f90a7bf52ce0ed485c12a1e7951595
Justin Sternberg <justin@dsgnwrks.pro>
2016-11-22 11:32:38 -0500

    Fix "Can't use method return value in write context" issue

--- includes/types/CMB2_Type_Title.php
+++ includes/types/CMB2_Type_Title.php
@@ -17,17 +17,17 @@ class CMB2_Type_Title extends CMB2_Type_Base {
 	 * @return string Heading element
 	 */
 	public function render() {
+		$name = $this->field->args( 'name' );
+		$tag  = 'span';
 
-		$tag = $this->field->object_type == 'post' ? 'h5' : 'h3';
-
-		if ( empty( $this->field->args( 'name' ) ) ) {
-			$tag = 'span';
+		if ( ! empty( $name ) ) {
+			$tag = $this->field->object_type == 'post' ? 'h5' : 'h3';
 		}
 
 		$a = $this->parse_args( 'title', array(
 			'tag'   => $tag,
-			'class' => empty( $this->field->args( 'name' ) ) ? 'cmb2-metabox-title-anchor' : 'cmb2-metabox-title',
-			'name'  => $this->field->args( 'name' ),
+			'class' => empty( $name ) ? 'cmb2-metabox-title-anchor' : 'cmb2-metabox-title',
+			'name'  => $name,
 			'desc'  => $this->_desc( true ),
 			'id'    => str_replace( '_', '-', sanitize_html_class( $this->field->id() ) ),
 		) );
