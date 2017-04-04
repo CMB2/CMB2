b36f3223c8451d5414ce2ad8c11385c523e76d13
Chris Reynolds <chris@jazzsequence.com>
2017-04-04 12:39:09 -0600

    closed_class will either be closed or empty, iterator should be an int

--- includes/CMB2.php
+++ includes/CMB2.php
@@ -492,7 +492,7 @@ class CMB2 extends CMB2_Base {
 		$closed_class = $field_group->options( 'closed' ) ? ' closed' : '';
 
 		echo '
-		<div class="postbox cmb-row cmb-repeatable-grouping', $closed_class, '" data-iterator="', $field_group->index, '">';
+		<div class="postbox cmb-row cmb-repeatable-grouping', wp_kses( $closed_class, array() ), '" data-iterator="', absint( $field_group->index ), '">';
 
 		if ( $field_group->args( 'repeatable' ) ) {
 			echo '<button type="button" ', $remove_disabled, 'data-selector="', $field_group->id(), '_repeat" class="dashicons-before dashicons-no-alt cmb-remove-group-row" title="', esc_attr( $field_group->options( 'remove_button' ) ), '"></button>';
