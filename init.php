7872670425cac58fc847d02f32e45e8172005d7b
Justin Sternberg <justin@dsgnwrks.pro>
2017-08-07 15:43:10 -0400

    Re-trunk the loader

--- init.php
+++ init.php
@@ -54,7 +54,7 @@
  * ***********************************************************************
  */
 
-if ( ! class_exists( 'CMB2_Bootstrap_2251', false ) ) {
+if ( ! class_exists( 'CMB2_Bootstrap_226_Trunk', false ) ) {
 
 	/**
 	 * Handles checking for and loading the newest version of CMB2
@@ -67,7 +67,7 @@ if ( ! class_exists( 'CMB2_Bootstrap_2251', false ) ) {
 	 * @license   GPL-2.0+
 	 * @link      https://cmb2.io
 	 */
-	class CMB2_Bootstrap_2251 {
+	class CMB2_Bootstrap_226_Trunk {
 
 		/**
 		 * Current version number
@@ -87,17 +87,17 @@ if ( ! class_exists( 'CMB2_Bootstrap_2251', false ) ) {
 		const PRIORITY = 9975;
 
 		/**
-		 * Single instance of the CMB2_Bootstrap_2251 object
+		 * Single instance of the CMB2_Bootstrap_226_Trunk object
 		 *
-		 * @var CMB2_Bootstrap_2251
+		 * @var CMB2_Bootstrap_226_Trunk
 		 */
 		public static $single_instance = null;
 
 		/**
-		 * Creates/returns the single instance CMB2_Bootstrap_2251 object
+		 * Creates/returns the single instance CMB2_Bootstrap_226_Trunk object
 		 *
 		 * @since  2.0.0
-		 * @return CMB2_Bootstrap_2251 Single instance object
+		 * @return CMB2_Bootstrap_226_Trunk Single instance object
 		 */
 		public static function initiate() {
 			if ( null === self::$single_instance ) {
@@ -189,6 +189,6 @@ if ( ! class_exists( 'CMB2_Bootstrap_2251', false ) ) {
 	}
 
 	// Make it so...
-	CMB2_Bootstrap_2251::initiate();
+	CMB2_Bootstrap_226_Trunk::initiate();
 
 }// End if().
