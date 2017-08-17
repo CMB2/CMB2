e676501efc639a23678d146db4657268760eb343
rogerlos <git@web.rogerlos.com>
2017-08-17 14:23:11 -0700

    Looks for config params within the 'options' array of field. Internal default and parsing methods

--- includes/types/CMB2_Type_Submit.php
+++ includes/types/CMB2_Type_Submit.php
@@ -1,10 +1,13 @@
 <?php
+
 /**
  * CMB submit field type, used by options pages.
  *
- * Called by field types: 'submit', 'reset', 'submit_and_reset'
+ * Called by field types: 'submit', 'reset', 'submit_and_reset'.
  *
- * @since  2.XXX
+ * Argument handling is a bit wonky as wp_parse_args cannot handle recursive arguments.
+ *
+ * @since     2.XXX
  *
  * @category  WordPress_Plugin
  * @package   CMB2
@@ -14,23 +17,104 @@
  */
 class CMB2_Type_Submit extends CMB2_Type_Base {
 	
-	public function render( $args = array(), $action = 'submit' ) {
+	public function render() {
 		
-		$args = empty( $args ) ? $this->args : $args;
+		$a   = $this->type_submit_parse_args( $this->args );
+		$ret = '';
 		
-		$a = $this->parse_args( $this->type, array(
-			'id'          => '',
-			'name'        => $action . '-cmb',
-			'text'        => $action == 'submit' ? __( 'Save', 'cmb2' ) : __( 'Reset', 'cmb2' ),
-			'wrap'        => false,
-			'button_type' => $action == 'submit' ? 'primary' : 'secondary',
-			'attributes'  => array(),
-		), $args );
-		
-		if ( ! empty( $a['id'] ) ) {
-			$a['attributes']['id'] = $a['id'];
+		foreach ( $a as $butt ) {
+			
+			if ( $butt[ 'text' ] ) {
+				
+				if ( ! empty( $butt[ 'id' ] ) ) {
+					$butt[ 'attributes' ]['id'] = $butt[ 'id' ];
+				}
+				
+				$ret .= get_submit_button(
+					esc_attr( __( $butt[ 'text' ] ) ),
+					$butt[ 'button' ],
+					$butt[ 'name' ],
+					$butt[ 'wrap' ],
+					$butt[ 'attributes' ]
+				);
+			}
 		}
 		
-		return get_submit_button( esc_attr( $a['text'] ), $a['button_type'], $a['name'], $a['wrap'], $a['attributes'] );
+		return $ret;
+	}
+	
+	/**
+	 * wp_parse_args is not recursive; need to merge the defaults before passing to base class parser
+	 *
+	 * @param $args
+	 *
+	 * @return array
+	 */
+	public function type_submit_parse_args( $args ) {
+		
+		foreach( $this->type_submit_defaults() as $button => $bargs ) {
+			
+			if ( isset( $args[ $button ] ) && is_array( $args[ $button ] ) ) {
+				
+				// make sure attributes which must be set have a value on the incoming options
+				$args[ $button ] = $this->type_submit_property_not_empty( $bargs, $args[ $button ] );
+				$args[ $button ] = array_merge( $bargs, $args[ $button ] );
+			}
+		}
+		
+		// expose arguments to allowing filtering in base method
+		return $this->parse_args( 'submit', $this->type_submit_defaults(), $args );
+	}
+	
+	/**
+	 * Makes sure that empty values were not passed via field
+	 *
+	 * @param array $checks
+	 * @param array $button_defaults
+	 * @param array $button_args
+	 *
+	 * @return array
+	 */
+	public function type_submit_property_not_empty( $button_defaults, $button_args, $checks = array() ) {
+		
+		$checks = empty( $checks ) ? array( 'name', 'button', 'wrap' ) : $checks;
+		
+		foreach( $checks as $check ) {
+			if ( empty( $button_args[ $check ] ) ) {
+				$button_args[ $check ] = $button_defaults[ $check ];
+			}
+		}
+		
+		return $button_args;
+	}
+	
+	/**
+	 * Set the defaults.
+	 *
+	 * @param array $defaults
+	 *
+	 * @return array
+	 */
+	public function type_submit_defaults( $defaults = array() ) {
+		
+		return ! empty( $defaults ) ? $defaults :
+			array(
+			'reset' => array(
+				'id' => '',
+				'name' => 'reset-cmb',
+				'text' => '',
+				'button' => 'secondary',
+				'wrap' => false,
+				'attributes' => array( 'style' => 'margin-right: 10px;' ),
+			),
+			'submit' => array(
+				'id' => '',
+				'name' => 'submit-cmb',
+				'text' => 'Save',
+				'button' => 'primary',
+				'wrap' => false,
+				'attributes' => array(),
+			),
+		);
 	}
 }
