1c7eaa0fa2049408210cfacf32cf008718035f0f
rogerlos <git@web.rogerlos.com>
2017-08-27 10:30:52 -0700

    Cleanup, descriptive method names, alphabetized methods

--- includes/CMB2_Options_Pages.php
+++ includes/CMB2_Options_Pages.php
@@ -18,55 +18,37 @@ class CMB2_Options_Pages {
 	 * @var array
 	 */
 	protected static $pages = array();
-
+	
 	/**
 	 * Add a CMB2_Options_Page_Hookup instance object to the registry.
 	 *
 	 * @since 2.XXX
-	 *
 	 * @param CMB2_Options_Page_Hookup $page
-	 *
 	 * @return string
 	 */
 	public static function add( CMB2_Options_Page_Hookup $page ) {
+		
 		self::$pages[ $page->page ] = $page;
+		
 		return $page->page;
 	}
 	
 	/**
-	 * Remove a CMB2_Options_Page_Hookup instance object from the registry.
-	 *
-	 * @since 2.XXX
-	 *
-	 * @param string $page A CMB2_Options_Page_Hookup instance id.
-	 *
-	 * @return bool
-	 */
-	public static function remove( $page ) {
-		if ( array_key_exists( $page, self::$pages ) ) {
-			unset( self::$pages[ $page ] );
-			return true;
-		}
-		return false;
-	}
-
-	/**
 	 * Retrieve a CMB2_Options_Page_Hookup instance by cmb id.
 	 *
 	 * @since 2.XXX
-	 *
-	 * @param string                   $page  A CMB2_Options_Page_Hookup instance id.
-	 *
+	 * @param string $page A CMB2_Options_Page_Hookup instance id.
 	 * @return CMB2_Options_Page_Hookup|bool  False or CMB2_Options_Page_Hookup object instance.
 	 */
 	public static function get( $page ) {
+		
 		if ( empty( self::$pages ) || empty( self::$pages[ $page ] ) ) {
-			return false;
+			return FALSE;
 		}
-
+		
 		return self::$pages[ $page ];
 	}
-
+	
 	/**
 	 * Retrieve all CMB2_Options_Page_Hookup instances registered.
 	 *
@@ -74,26 +56,45 @@ class CMB2_Options_Pages {
 	 * @return CMB2_Options_Page_Hookup[] Array of all registered CMB2_Options_Page_Hookup instances.
 	 */
 	public static function get_all() {
+		
 		return self::$pages;
 	}
-
+	
 	/**
 	 * Retrieve all CMB2_Options_Page_Hookup instances that have the same options key.
 	 *
 	 * @since  2.XXX
-	 * @param  string $key  Key matching options-key
+	 * @param  string $key Key matching options-key
 	 * @return CMB2[]       Array of matching CMB2_Options_Page_Hookup instances
 	 */
-	public static function options_key_pages( $key ) {
+	public static function get_by_options_key( $key ) {
 		
 		$pages = array();
-
+		
 		foreach ( self::$pages as $id => $page ) {
 			if ( $page->option_key === $key ) {
 				$pages[ $id ] = $page;
 			}
 		}
-
+		
 		return $pages;
+	}
+	
+	/**
+	 * Remove a CMB2_Options_Page_Hookup instance object from the registry.
+	 *
+	 * @since 2.XXX
+	 * @param string $page A CMB2_Options_Page_Hookup instance id.
+	 * @return bool
+	 */
+	public static function remove( $page ) {
+		
+		if ( array_key_exists( $page, self::$pages ) ) {
+			unset( self::$pages[ $page ] );
+			
+			return TRUE;
+		}
+		
+		return FALSE;
 	}
 }
