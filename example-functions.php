1e1e3c26ca8e3b0993070b666e1a5233c34581fb
Justin Sternberg <justin@dsgnwrks.pro>
2017-11-14 11:19:58 -0500

    Clean up some malformed doc blocks in the example file

--- example-functions.php
+++ example-functions.php
@@ -24,9 +24,9 @@ if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
 /**
  * Conditionally displays a metabox when used as a callback in the 'show_on_cb' cmb2_box parameter
  *
- * @param  CMB2 object $cmb CMB2 object.
+ * @param  CMB2 $cmb CMB2 object.
  *
- * @return bool             True if metabox should show
+ * @return bool      True if metabox should show
  */
 function yourprefix_show_if_front_page( $cmb ) {
 	// Don't show this metabox if it's not the front page template.
@@ -39,9 +39,9 @@ function yourprefix_show_if_front_page( $cmb ) {
 /**
  * Conditionally displays a field when used as a callback in the 'show_on_cb' field parameter
  *
- * @param  CMB2_Field object $field Field object.
+ * @param  CMB2_Field $field Field object.
  *
- * @return bool                     True if metabox should show
+ * @return bool              True if metabox should show
  */
 function yourprefix_hide_if_no_cats( $field ) {
 	// Don't show this field if not in the cats category.
@@ -91,8 +91,8 @@ function yourprefix_display_text_small_column( $field_args, $field ) {
 /**
  * Conditionally displays a message if the $post_id is 2
  *
- * @param  array             $field_args Array of field parameters.
- * @param  CMB2_Field object $field      Field object.
+ * @param  array      $field_args Array of field parameters.
+ * @param  CMB2_Field $field      Field object.
  */
 function yourprefix_before_row_if_2( $field_args, $field ) {
 	if ( 2 == $field->object_id ) {
