3596a2ed7188ba18f1197157854e8bcd360fd125
Justin Sternberg <justin@dsgnwrks.pro>
2018-05-21 20:59:17 -0400

    Add some travis-folds for better readability

--- tests/bin/run-wp-unit-tests.sh
+++ tests/bin/run-wp-unit-tests.sh
@@ -4,13 +4,18 @@ cd "$(dirname "$0")/../../"
 
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 
+echo -en "travis_fold:start:install_wp_tests\r"
 bash tests/bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
+echo -en "travis_fold:end:install_wp_tests\r"
+
 source tests/bin/install-php-phpunit.sh
 
 if [[ ! -z "$CC_TEST_REPORTER_ID" ]] && [[ ! -z $(php -i | grep xdebug) ]]; then
+	echo -en "travis_fold:start:start_codeclimate_reporter\r"
 	curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 	chmod +x ./cc-test-reporter
 	./cc-test-reporter before-build
+	echo -en "travis_fold:end:start_codeclimate_reporter\r"
 fi
 
 echo "Running with the following versions:"
