d5bf298a384efa4bb0e53ad2104a64065820c49c
Justin Sternberg <justin@dsgnwrks.pro>
2018-12-10 11:52:14 -0500

    Add develop class suffix to init.php class

--- init.php
+++ init.php
@@ -53,7 +53,7 @@
  * ***********************************************************************
  */
 
-if ( ! class_exists( 'CMB2_Bootstrap_251', false ) ) {
+if ( ! class_exists( 'CMB2_Bootstrap_251_Develop', false ) ) {
 
 	/**
 	 * Handles checking for and loading the newest version of CMB2
@@ -66,7 +66,7 @@ if ( ! class_exists( 'CMB2_Bootstrap_251', false ) ) {
 	 * @license   GPL-2.0+
 	 * @link      https://cmb2.io
 	 */
-	class CMB2_Bootstrap_251 {
+	class CMB2_Bootstrap_251_Develop {
 
 		/**
 		 * Current version number
@@ -86,17 +86,17 @@ if ( ! class_exists( 'CMB2_Bootstrap_251', false ) ) {
 		const PRIORITY = 9965;
 
 		/**
-		 * Single instance of the CMB2_Bootstrap_251 object
+		 * Single instance of the CMB2_Bootstrap_251_Develop object
 		 *
-		 * @var CMB2_Bootstrap_251
+		 * @var CMB2_Bootstrap_251_Develop
 		 */
 		public static $single_instance = null;
 
 		/**
-		 * Creates/returns the single instance CMB2_Bootstrap_251 object
+		 * Creates/returns the single instance CMB2_Bootstrap_251_Develop object
 		 *
 		 * @since  2.0.0
-		 * @return CMB2_Bootstrap_251 Single instance object
+		 * @return CMB2_Bootstrap_251_Develop Single instance object
 		 */
 		public static function initiate() {
 			if ( null === self::$single_instance ) {
@@ -188,6 +188,6 @@ if ( ! class_exists( 'CMB2_Bootstrap_251', false ) ) {
 	}
 
 	// Make it so...
-	CMB2_Bootstrap_251::initiate();
+	CMB2_Bootstrap_251_Develop::initiate();
 
 }// End if().
