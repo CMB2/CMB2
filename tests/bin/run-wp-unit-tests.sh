75214cf1268c0586cb0b1293a5ef6cb544c03552
Nguyen Van Anh <anhskohbo@gmail.com>
2019-01-23 22:50:53 +0700

    install phpunit via composer

--- tests/bin/run-wp-unit-tests.sh
+++ tests/bin/run-wp-unit-tests.sh
@@ -8,7 +8,7 @@ echo -en "travis_fold:start:install_wp_tests\r"
 bash tests/bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
 echo -en "travis_fold:end:install_wp_tests\r"
 
-source tests/bin/install-php-phpunit.sh
+composer install --prefer-dist --no-suggest
 
 if [[ ! -z "$CC_TEST_REPORTER_ID" ]] && [[ ! -z $(php -i | grep xdebug) ]]; then
 	echo -en "travis_fold:start:start_codeclimate_reporter\r"
@@ -20,11 +20,11 @@ fi
 
 echo "Running with the following versions:"
 php -v
-phpunit --version
+vendor/bin/phpunit --version
 
 # Run PHPUnit tests
 if [[ latest == $WP_VERSION ]]; then
-	phpunit --coverage-clover=clover.xml || exit 1;
+	vendor/bin/phpunit --coverage-clover=clover.xml || exit 1;
 else
-	phpunit --exclude-group cmb2-rest-api --coverage-clover=clover.xml || exit 1;
+	vendor/bin/phpunit --exclude-group cmb2-rest-api --coverage-clover=clover.xml || exit 1;
 fi
